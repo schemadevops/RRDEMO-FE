0b16dfc7669804d6d498ec94f9780d29c9de73a1
refiahmadfauzi <34942113+refiahmadfauzi@users.noreply.github.com>
2023-07-13 14:40:05 +0700

    update bgcolor 404

--- application/views/errors/custom404_view.php
+++ application/views/errors/custom404_view.php
@@ -40,11 +40,11 @@
     <script src="<?= base_url('assets/'); ?>assets/js/config.js"></script>
 </head>
 
-<body>
+<body style="background-color: #CAF1EF;">
     <!-- Content -->
 
     <!--Under Maintenance -->
-    <div class="container-xxl">
+    <div>
         <div class="misc-wrapper">
             <h2 class="mb-2 mx-2">Under Maintenance!</h2>
             <p class="mb-4 mx-2">Sorry for the inconvenience but we're performing some maintenance at the moment</p>
