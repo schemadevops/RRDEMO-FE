704c8e81383dd24dedd1c227fc51802965233d16
refiahmadfauzi <34942113+refiahmadfauzi@users.noreply.github.com>
2023-08-02 14:44:22 +0700

    apolo form 0008

--- application/controllers/Apollo.php
+++ application/controllers/Apollo.php
@@ -1345,11 +1345,149 @@ class Apollo extends CI_Controller
 
 	public function form_report_isi_0008()
 	{
+		$tgl_seacrh = $this->session->tgl_periode;
+
+		if ($tgl_seacrh != "") {
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => 'http://141.136.47.149:3003/apolo/form0008/getbydate',
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'POST',
+				CURLOPT_POSTFIELDS => 'tanggal=' . $tgl_seacrh,
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/x-www-form-urlencoded',
+					'Authorization: Bearer ' . $this->session->access_token
+				),
+			));
+
+			$response = curl_exec($curl);
+
+			curl_close($curl);
+			$hasil = json_decode($response);
+			if ($hasil->message == "success") {
+				$data['api_hasil'] = $hasil->data;
+			}
+			$this->session->unset_userdata('tgl_periode');
+		} else {
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => 'http://141.136.47.149:3003/apolo/form0008',
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_HTTPHEADER => array(
+					'Authorization: Bearer ' . $this->session->access_token
+				),
+			));
+
+			$response = curl_exec($curl);
+
+			curl_close($curl);
+			$hasil = json_decode($response);
+			if ($hasil->message == "success") {
+				$data['api_hasil'] = $hasil->data;
+			}
+		};
+
+		$newdata = array(
+			'data_export_txt'  => $data['api_hasil'],
+			'kode_form'		=> '00.08',
+			'nama_form'		=> 'Form 00.08 Quarterly Financila Rations'
+		);
+		$this->session->set_userdata($newdata);
+
+		$data['api_hasil'] = $hasil;
+		$data['header'] = 'Form 00.08 Quarterly Financila Rations';
+
 		$this->load->view('temp/head');
 		$this->load->view('temp/sidebar');
 		$this->load->view('temp/navbar');
-		$this->load->view('apollo/form_report_isi_08');
+		$this->load->view('apollo/form_report_isi_08', $data);
 	}
+
+	public function form_report_edit_08($id)
+	{
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'http://141.136.47.149:3003/apolo/form0008/' . $id,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+			CURLOPT_HTTPHEADER => array(
+				'Authorization: Bearer ' . $this->session->access_token
+			),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		$hasil = json_decode($response);
+		if ($hasil->message == "success") {
+			$data['api_hasil'] = $hasil->data;
+			$data['api_log_data'] = $hasil->logData;
+		}
+
+		$data['header'] = "Apolo Form 00.08 Quarterly Financila Rations";
+		$this->load->view('temp/head');
+		$this->load->view('temp/sidebar');
+		$this->load->view('temp/navbar');
+		$this->load->view('apollo/form_report_edit_08', $data);
+	}
+
+	public function ajax_edit_0008()
+	{
+		$id_0008 = $this->input->post('id_0008');
+		$nilai_rasio = $this->input->post('nilai_rasio');
+		$jenis_rasio = $this->input->post('jenis_rasio');
+		$sandi = $this->input->post('sandi');
+		$alasan_edit = $this->input->post('alasan_edit');
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'http://141.136.47.149:3003/apolo/form0008/' . $id_0008,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'PUT',
+			CURLOPT_POSTFIELDS => 'jenis_rasio=' . $jenis_rasio . '&sandi=' . $sandi . '&nilai_rasio=' . $nilai_rasio . '&alasan=' . $alasan_edit,
+			CURLOPT_HTTPHEADER => array(
+				'Content-Type: application/x-www-form-urlencoded',
+				'Authorization: Bearer ' . $this->session->access_token
+			),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		$hasil = json_decode($response);
+
+		if ($hasil->message == "success") {
+			$data['api_hasil'] = $hasil->data;
+			echo json_encode(array("status" => TRUE, "data" => $data));
+		}
+	}
+
+
 	public function form_report_isi_0009()
 	{
 		$this->load->view('temp/head');
@@ -1521,13 +1659,7 @@ class Apollo extends CI_Controller
 
 
 
-	public function form_report_edit_08()
-	{
-		$this->load->view('temp/head');
-		$this->load->view('temp/sidebar');
-		$this->load->view('temp/navbar');
-		$this->load->view('apollo/form_report_edit_08');
-	}
+
 	public function form_report_edit_09()
 	{
 		$this->load->view('temp/head');
--- application/views/apollo/form_report_isi_08.php
+++ application/views/apollo/form_report_isi_08.php
@@ -6,97 +6,62 @@
         <div class="row">
             <!-- Basic Layout & Basic with Icons -->
             <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Apolo /</span>
-                Form Report/ Form 00.08</h4>
+                Form Report/ <?= $header; ?></h4>
             <!-- Responsive Table -->
             <div class="card">
-                <h5 class="card-header">Form 00.08</h5>
+                <h5 class="card-header"><?= $header; ?></h5>
                 <div class="container mb-3 py-3">
-                    <form>
-                        <div class="row mb-3">
-                            <label class="col-sm-3 col-form-label" for="basic-default-name">Periode
+                    <form id="form_periode">
+                        <div class="row mb-3 mt-3">
+                            <label class="col-sm-3 col-form-label" for="periode_date">Periode
                                 Data</label>
                             <div class="col-sm-6">
-                                <input type="date" class="form-control" id="basic-default-name" placeholder="John Doe" />
+                                <input type="date" class="form-control" id="periode_date" name="periode_date" placeholder="John Doe" />
                             </div>
                             <div class="col-sm-3">
-                                <button type="submit" class="btn btn-primary">Search</button>
+                                <button type="button" onclick="save_periode()" id="btnSeacrh" class="btn btn-primary">Search</button>
+                                <button type="button" onclick="reload_table()" id="btnReload" class="btn btn-warning">Reset</button>
                             </div>
                         </div>
                     </form>
-
                 </div>
                 <div class="table-responsive text-nowrap">
-                    <table class="table">
+                    <table class="table dt-responsive" id="table-res">
                         <thead>
                             <tr class="text-nowrap">
                                 <th>No</th>
                                 <th>Jenis Rasio</th>
                                 <th>Sandi</th>
                                 <th>Nilai Rasio (%)</th>
+                                <th>Created At</th>
                                 <th>Edit</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Kewajiban Penyediaan Modal Minimum (KPMM)</td>
-                                <td>0101</td>
-                                <td>-5.55</td>
-                                <td>
-                                    <a href="<?= base_url('apollo/form_report_edit_08'); ?>" class="btn rounded-pill btn-primary"><i class='bx bx-edit-alt'></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Kualitas Aset Produktif (KAP)</td>
-                                <td>0201</td>
-                                <td>5.56</td>
-                                <td>
-                                    <a href="<?= base_url('apollo/form_report_edit_08'); ?>" class="btn rounded-pill btn-primary"><i class='bx bx-edit-alt'></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Penyisihan Penghapusan Aset Produktif (PPAP)</td>
-                                <td>0202</td>
-                                <td>5.57</td>
-                                <td>
-                                    <a href="<?= base_url('apollo/form_report_edit_08'); ?>" class="btn rounded-pill btn-primary"><i class='bx bx-edit-alt'></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td>NPL (Neto)</td>
-                                <td>0203</td>
-                                <td>-5.58</td>
-                                <td>
-                                    <a href="<?= base_url('apollo/form_report_edit_08'); ?>" class="btn rounded-pill btn-primary"><i class='bx bx-edit-alt'></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">5</th>
-                                <td>Non Performing Loan (NPL) Gross</td>
-                                <td>0204</td>
-                                <td>5.59</td>
-                                <td>
-                                    <a href="<?= base_url('apollo/form_report_edit_08'); ?>" class="btn rounded-pill btn-primary"><i class='bx bx-edit-alt'></i>
-                                    </a>
-                                </td>
-                            </tr>
-
-
+                            <?php
+                            $no = 0;
+                            foreach ($api_hasil->data as $key) {
+                                $no++;
+                            ?>
+                                <tr>
+                                    <th scope="row"><?= $no; ?></th>
+                                    <td><?= $key->jenis_rasio; ?></td>
+                                    <td><?= $key->sandi; ?></td>
+                                    <td><?= $key->nilai_rasio; ?></td>
+                                    <td><?= $key->createdAt; ?></td>
+                                    <td>
+                                        <a href="<?= base_url('apollo/form_report_edit_08/' . $key->id); ?>" class="btn rounded-pill btn-primary"><i class='bx bx-edit-alt'></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php };
+                            ?>
                         </tbody>
                     </table>
                 </div>
                 <div class="row mt-3 mb-3">
                     <div class="col-md-3">
-                        <a href="#" class="btn rounded-pill btn-primary my-2">Export to Excel
-                        </a>
-                        <a href="#" class="btn rounded-pill btn-primary my-2">Export to Txt
+                        <a href="<?= base_url('apollo/exportDataToTxt'); ?>" class="btn rounded-pill btn-primary my-2">Export to Txt
                         </a>
                     </div>
                     <div class="col-md-6">
@@ -118,3 +83,86 @@
 </div>
 
 <?php $this->view('temp/footer'); ?>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#table-res').DataTable({
+            dom: 'Bfrtip',
+            buttons: [{
+                    extend: 'copyHtml5',
+                    messageTop: 'APOLO - Form 00.08 Quarterly Financila Rations',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3]
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    messageTop: 'APOLO - Form 00.08 Quarterly Financila Rations',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3]
+                    }
+                },
+                'colvis'
+            ]
+        });
+    });
+
+    function save_periode() {
+        $('#btnSeacrh').text('searching...'); //change button text
+        $('#btnSeacrh').attr('disabled', true); //set button disable 
+        var url;
+
+        url = "<?php echo site_url('apollo/ajax_periode') ?>";
+
+
+        // ajax adding data to database
+
+        var formData = new FormData($('#form_periode')[0]);
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            dataType: "JSON",
+            success: function(data) {
+                if (data.status) //if success close modal and reload ajax table
+                {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Data Berhasil dicari'
+                    });
+                    window.setTimeout(function() {
+                        location.reload();
+                    }, 1000);
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Opss ada yang error'
+                    })
+                }
+                $('#btnSeacrh').text('Search'); //change button text
+                $('#btnSeacrh').attr('disabled', false); //set button enable 
+
+
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert('Error adding / update data');
+                $('#btnSeacrh').text('Search'); //change button text
+                $('#btnSeacrh').attr('disabled', false); //set button enable 
+
+            }
+        });
+    }
+
+    function reload_table() {
+        window.setTimeout(function() {
+            location.reload();
+        }, 1000);
+    }
+</script>
